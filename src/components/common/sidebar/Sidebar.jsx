24ad87cc18f3143c482a173542e6f0c671ef7a27
Neampi <piero.neam@gmail.com>
2024-04-30 16:46:09 -0500

    5

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -6,12 +6,12 @@ import './sidebar.css'
 const Sidebar = () => {
   return (
     <header className='head-side'>
-        <div className="logo">LOGO</div>
+        <div className="logo"><img className='logo1' src="./logo.png"/><img className='logo2' src="./logoicon.png"/></div>
         <div className="views">
             <nav>
                 <ul>
-                    <li><Link to="/"><p className='name-side'>Home</p></Link></li>
-                    <li><Link to="/view1"><p className='name-side'>Vista 1</p></Link></li>
+                    <Link to="/"><li><img src="./home.png" alt="" /><p className='name-side'>Home</p></li></Link>
+                    <Link to="/view1"><li><img src="./view.png" alt="" /><p className='name-side'>Vista 1</p></li></Link>
                 </ul>
             </nav>
         </div>
